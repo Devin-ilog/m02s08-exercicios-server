aa2f71d9e6e9ac76c710a3fff53d8969584320d3
zanoniFab <fabianezanoni@gmail.com>
2023-03-18 13:57:32 -0300

    exercicio 4: criar card para os produtos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 import './App.css'
 import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
+import { Card } from "./components/Card";
+
+const prato1 = {
+  imagem: "http://localhost:8081/assets/salada-jacare.png",
+  nome: "Salada de Jacaré",
+  descricao: "Mix de folhas coloridas, tomate cereja,croutôns, palmito e lascas de jacaré salteadas, regadas ao molho balsâmico, oliva e mel.",
+  valor: 49,
+  tempoPreparo: "10 min",
+}
 
 function App() {
 
@@ -9,12 +18,13 @@ function App() {
       <Navbar />
       
       <main className='main'>
+        <h2>Produtos</h2>
+        
+      </main>
 
       <Footer />
 
-      </main>
-
-          </div>
+    </div>
   )
 }
 
---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import "./Card.css";
+
+export const Card = ({nome, imagem, descricao, valor, tempoPreparo}) => {
+
+    return (
+        <div className='card'>
+            <img width={200} src={imagem} alt="imagem do produto"/>
+            <div className="card-info">
+                <h3 className="card-title">{nome}</h3>
+
+                <p className="card-description">{descricao}</p>
+
+                <div className='card-footer'>
+                    <p>{`R$ ${valor}`}</p>
+                    <p>{tempoPreparo}</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
