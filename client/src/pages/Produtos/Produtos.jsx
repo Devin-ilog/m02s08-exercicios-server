91ab174c3eab2aa25221a9f5668050cf01a79343
zanoniFab <fabianezanoni@gmail.com>
2023-03-19 19:40:13 -0300

    exercicio 6: utilizar o hook criado, exibir loading ou erro se tiver, ou a lista com os itens caso não tenha erro

--- client/src/pages/Produtos/Produtos.jsx
+++ client/src/pages/Produtos/Produtos.jsx
@@ -2,25 +2,36 @@ import { Card } from '../../components/Card'
 import { useProdutos } from '../../hooks/useProdutos'
 import './Produtos.css'
 
-const prato1 = {
-    imagem: "http://localhost:8081/assets/salada-jacare.png",
-    nome: "Salada de Jacaré",
-    descricao: "Mix de folhas coloridas, tomate cereja,croutôns, palmito e lascas de jacaré salteadas, regadas ao molho balsâmico, oliva e mel.",
-    valor: 49,
-    tempoPreparo: "10 min",
-  }
-
 export const Produtos = () => {
     const {error, isLoading, produtos} = useProdutos();
-    console.log(produtos)
+
+    if(isLoading) {
+        return <p>Carregando...</p>
+    }
+
+    if(error) {
+        return <p>{error}</p>
+    }
 
     return (
-        <section className='secao'>
-            <h2 className='secao-titulo'>Produtos</h2>
-            <hr/>
-            <div className='lista'>
-                <Card imagem={prato1.imagem} nome={prato1.nome} descricao={prato1.descricao} valor={prato1.valor} tempoPreparo={prato1.tempoPreparo}/>
-            </div>
-        </section>
+        <>
+            {!!produtos && Object.keys(produtos).map((secao) =>(
+                <section key={secao} className='secao'>
+                    <h2 className='secao-titulo'>{secao}</h2>
+                    <hr/>
+                    <div className='lista'>
+                        {produtos[secao].map((item)=> (
+                            <Card key={item.id}
+                                imagem={item.imagem}
+                                nome={item.nome} 
+                                descricao={item.descricao}
+                                valor={item.valor}
+                                tempoPreparo={item.tempoPreparo}/>
+                        ))}
+                    </div>
+                </section>
+            ))}
+                
+        </>
     )
 }
